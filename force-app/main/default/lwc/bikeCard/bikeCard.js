55ad5311e1f2c7e26725778cd6028ab9e559e118
shravya-Dev-Per <shravya.pottamsetty@appsassociates.com>
2023-08-03 16:20:33 +0530

    changed material

--- force-app/main/default/lwc/bikeCard/bikeCard.js
+++ force-app/main/default/lwc/bikeCard/bikeCard.js
@@ -3,7 +3,7 @@ export default class BikeCard extends LightningElement {
    name = 'Electra X4';
    description = 'A sweet bike built for comfort.';
    category = 'Mountain';
-   material = 'Steel';
+   material = 'Steelr';
    price = '$2,700';
    pictureUrl = 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg';
  }
